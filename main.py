b76b01d0dc916a7750b41ab86206a72ec04fd31f
Tiko4real <166095197+Tiko4real@users.noreply.github.com>
2024-07-02 17:05:23 +0200

    Initial commit with project setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+class BattleshipGame:
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+        self.player_board = [['~' for _ in range(grid_size)] for _ in range(grid_size)]
+        self.computer_board = [['~' for _ in range(grid_size)] for _ in range(grid_size)]
+        self.ships = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine': 3, 'Destroyer': 2}
+        self.place_ships()
+
+    def place_ships(self):
+        for size in self.ships.values():
+            self.place_ship_randomly(size)
